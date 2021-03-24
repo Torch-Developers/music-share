af0658d268de37800bf24544ced977f5c8314fe2
dantebarnes <dante@longsoftware.io>
2021-03-24 12:20:11 -0400

    add eslint, prettier, husky, lint files

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,23 @@
 // db.js
 
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require('mongoose')
+// eslint-disable-next-line import/no-extraneous-dependencies
+const config = require('config')
+const db = config.get('mongoURI')
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(
-            db,
-            {
-                useNewUrlParser: true
-            }
-        );
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    })
 
-        console.log('MongoDB is Connected...');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-};
+    // eslint-disable-next-line no-console
+    console.log('MongoDB is Connected...')
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err.message)
+    process.exit(1)
+  }
+}
 
-module.exports = connectDB;
+module.exports = connectDB
