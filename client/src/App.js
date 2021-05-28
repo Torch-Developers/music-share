ba22c1d7ed87398e1cf67d0556e3844448884d30
Collin Lanier <collinlanier12@gmail.com>
2021-05-28 13:16:56 -0400

    fixed tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Route, Switch } from "react-router"
+import { BrowserRouter } from 'react-router-dom'
 
 import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <div className="App">
+    <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Home} />
       </Switch>
+    </BrowserRouter>
     </div>
   )
 }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,10 @@
+import {render, screen } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+test('renders learn react link', () => {
+  render(<App />)
+  const linkElement = screen.getByText("Music Share");
+  expect(linkElement).toBeInTheDocument()
+}) 
